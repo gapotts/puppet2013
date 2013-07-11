e8e2afb12814fc19ae1ab751929ee4a4f7743aab
Chris Barbour <cbarbour@taos.com>
2013-07-11 14:31:20 +0000

    Enabled autosigning

--- modules/puppet_master/manifests/config.pp
+++ modules/puppet_master/manifests/config.pp
@@ -13,6 +13,13 @@ class puppet_master::config {
     source => 'puppet:///modules/puppet_master/etc/puppet/puppet.conf',
   }
 
+  file { '/etc/puppet/autosign.conf':
+    owner  => 'puppet',
+    group  => 'puppet',
+    mode   => '0444',
+    source => 'puppet:///modules/puppet_master/etc/puppet/autosign.conf',
+  }
+
   file { '/etc/sysconfig/puppetmaster':
     owner  => 'puppet',
     group  => 'puppet',
